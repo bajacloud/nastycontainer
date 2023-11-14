152b66f32c2db766205d492b74657e6c4a5d7c44
Matias Vera <matiasvera@Matiass-MacBook-Pro-2.local>
2023-11-13 20:31:13 -0500

    added timeouts

--- app/run_scenarios.sh
+++ app/run_scenarios.sh
@@ -1,35 +1,30 @@
 #!/bin/bash
 
 # Scenario 2: Compiler Executed In Container
-/app/scenarios/compiler_executed.sh
-sleep 5
+timeout 5s /app/scenarios/compiler_executed.sh
 
 # Scenario 3: 
-/app/scenarios/container_management_utility.sh
-sleep 5
+timeout 5s /app/scenarios/container_management_utility.sh
 
 # Scenario 4: Modify User Password (Ubuntu)
-/app/scenarios/modify_user_password.sh
-sleep 5
+timeout 5s /app/scenarios/modify_user_password.sh
 
 # Scenario 5:
-/app/scenarios/run_cryptominer.sh
-sleep 10
+timeout 10s /app/scenarios/run_cryptominer.sh
 
 # Scenario 6:
-/app/scenarios/run_malware.sh
-sleep 10
+timeout 10s /app/scenarios/run_malware.sh
 
 # Scenario 8: Container Management Utility in Container
-/app/scenarios/simulate_port_scan.sh
-sleep 10
+timeout 10s /app/scenarios/simulate_port_scan.sh
 
 # Scenario 8: Container Management Utility in Container
-/app/scenarios/start_network_capture.sh
-sleep 10
+timeout 10s /app/scenarios/start_network_capture.sh
+
 # Scenario 1: 
-/app/scenarios/bad_reputation_ip.sh
+timeout 10s /app/scenarios/bad_reputation_ip.sh
 
 # Sleep for a while to allow scenarios to run
 sleep 3600  # Adjust the duration as needed
+
 
