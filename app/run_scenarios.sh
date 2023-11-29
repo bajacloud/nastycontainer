e6c30bfd66a8e790bf84171532a101739f5fb98f
Matias Vera <matiasvera@Matiass-MacBook-Pro-2.local>
2023-11-28 21:04:30 -0500

    rename

--- app/run_scenarios.sh
+++ app/run_scenarios.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-# Scenario 2: Compiler Executed In Container
+# Scenario 1: Compiler Executed In Container
 timeout 5s /app/scenarios/compiler_executed.sh
 
-# Scenario 3: 
+# Scenario 2: Container Management Utility (kubectl)
 timeout 5s /app/scenarios/container_management_utility.sh
 
-# Scenario 4: Modify User Password (Ubuntu)
+# Scenario 3: Modify User Password (Ubuntu)
 timeout 5s /app/scenarios/modify_user_password.sh
 
-# Scenario 5:
+# Scenario 4: Cryptominer
 timeout 10s /app/scenarios/run_cryptominer.sh
 
-# Scenario 6:
+# Scenario 5: Malware
 timeout 10s /app/scenarios/run_malware.sh
 
-# Scenario 8: Container Management Utility in Container
+# Scenario 6: Port Scan
 timeout 10s /app/scenarios/simulate_port_scan.sh
 
-# Scenario 8: Container Management Utility in Container
+# Scenario 7: Network Capture
 timeout 10s /app/scenarios/start_network_capture.sh
 
-# Scenario 1: 
+# Scenario 8: Bad Reputation IPs
 timeout 10s /app/scenarios/bad_reputation_ip.sh
 
-# Scenario
+# Scenario 9: Reverse shell
 /app/scenarios/reverse_shell.sh
 
 # Sleep for a while to allow scenarios to 
