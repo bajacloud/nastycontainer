563c298d903bde4a8e0ec8f0a74e04a9af7b6cdd
Steven Feltner <57229654+stevenfeltner@users.noreply.github.com>
2024-01-08 23:17:38 -0800

    Update bad_reputation_ip.sh

--- app/scenarios/bad_reputation_ip.sh
+++ app/scenarios/bad_reputation_ip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl https://gist.githubusercontent.com/stevenfeltner/9cccd471a3887e6965b60a96c11aa651/raw/8187e18b47a7b849ba3c07d20a3a9f198a48e477/ping_bad.sh --output ping_bad.sh && chmod +x ping_bad.sh && ./ping_bad.sh
+curl https://gist.githubusercontent.com/stevenfeltner/9cccd471a3887e6965b60a96c11aa651/raw/04c3ea0d4b5eaa237640105ed50cf6b4fc7acb77/ping_bad.sh --output ping_bad.sh && chmod +x ping_bad.sh && ./ping_bad.sh
