66b1f282a6b04c5c9d869e3d75c2e23ef1d021a1
steven.feltner <57229654+stevenfeltner@users.noreply.github.com>
2024-01-09 17:02:16 -0800

    Dynamicly fetch the bad ips

--- app/scenarios/network/bad_reputation_ip.sh
+++ app/scenarios/network/bad_reputation_ip.sh
@@ -4,28 +4,9 @@
 # Policy: Bad reputation IP / Domains
 # A container is performing DNS lookups for bad reputation domains
 
-# List of IP addresses to ping
-IP_LIST=("170.64.210.160"
-"170.64.210.205"
-"170.64.210.207"
-"170.64.210.165"
-"222.186.16.214"
-"170.64.133.232"
-"211.253.10.96"
-"162.142.125.220"
-"82.200.65.218"
-"110.45.145.194"
-"1.214.214.114"
-"200.76.53.187"
-"150.95.64.109"
-"43.155.141.65"
-"66.98.112.247"
-"210.87.195.112"
-"165.154.236.136"
-"113.200.60.74"
-"101.36.107.47"
-"186.209.77.237"
-"167.248.133.36")
+# Fetch the most recent reported bad IPs. Take the top 10.
+wget https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt
+IP_LIST=($(awk '!/^#/ && NR>4 && NR<=14 {print "\"" $1 "\""}' ipsum.txt))
 
 # Number of times to repeat the list
 NUM_REPEATS=1
